ea8d79622f5c6cbfd3ba757bfe18d5c72011e91a
MrKiniko <kiniko99@gmail.com>
2025-08-09 16:35:23 +0200

    quitar archivos innecesarios

--- waybar/scripts/nowplaying.sh
+++ waybar/scripts/nowplaying.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# .        :  :::::::..    :::  .   ::::::.    :::.::: :::  .      ...     
+# ;;,.    ;;; ;;;;``;;;;   ;;; .;;,.;;;`;;;;,  `;;;;;; ;;; .;;,..;;;;;;;.  
+# [[[[, ,[[[[, [[[,/[[['   [[[[[/'  [[[  [[[[[. '[[[[[ [[[[[/' ,[[     \[[,
+# $$$$$$$$"$$$ $$$$$$c    _$$$$,    $$$  $$$ "Y$c$$$$$_$$$$,   $$$,     $$$
+# 888 Y88" 888o888b "88bo,"888"88o, 888  888    Y88888"888"88o,"888,_ _,88P
+# MMM  M'  "MMMMMMM   "W"  MMM "MMP"MMM  MMM     YMMMM MMM "MMP" "YMMMMMP" 
+# 
+
 # Función para escapar caracteres especiales para Pango markup
 escape_markup() {
   local input="$1"
