2155d374a1fbf84cc11135929f477c4282a0951a
MrKiniko <kiniko99@gmail.com>
2025-08-11 15:53:03 +0200

    añadido change2blue.sh y atajos de teclado a este en hyprland.conf

---
+++ change2blue.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp -r /home/kino/Documentos/colores\ dotfiles/blue/* /home/kino/dev/config/dotfiles
+
+pkill -SIGUSR2 waybar
+
+pkill swaync
+swaync &
+
+pkill -USR1 hyprpaper
+hyprpaper &
